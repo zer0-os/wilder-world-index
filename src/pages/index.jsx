5ef6d7d35d22d00541e4aecec7755f7756c3d342
Akshat Mittal <itsreallyakshat@gmail.com>
2021-03-23 08:05:17 +0530

    Meta tags

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -18,6 +18,22 @@ export default function Home() {
       <Head>
         <title>Wilder World</title>
         <link rel="icon" href="/favicon.ico" />
+        <meta name="description" content="Decentralized NFT Market and Artist Guild" />
+
+        <meta name="twitter:card" content="summary" />
+        <meta name="twitter:site" content="@wilderworld_" />
+        <meta name="twitter:title" content="Wilder World" />
+        <meta name="twitter:description" content="Decentralized NFT Market and Artist Guild" />
+        <meta name="twitter:creator" content="@iakshatmittal" />
+        <meta name="twitter:image" content="https://www.wilderworld.com/assets/background/wilder.png" />
+
+        <meta property="og:title" content="Wilder World" />
+        <meta property="og:type" content="article" />
+        <meta property="og:url" content="https://www.wilderworld.com" />
+        <meta property="og:image" content="https://www.wilderworld.com/assets/background/wilder.png" />
+        <meta property="og:description" content="Decentralized NFT Market and Artist Guild" />
+        <meta property="og:site_name" content="Wilder World" />
+        <meta property="fb:admins" content="100000581280466" />
       </Head>
       <LoaderStore>
         <Loader />
