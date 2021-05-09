cb277c9e554e2b8f2a2c924ac9c5978e28f1edbb
Akshat Mittal <itsreallyakshat@gmail.com>
2021-05-09 15:31:27 +0530

    Update CyberHeist

--- src/pages/cyberheist/index.jsx
+++ src/pages/cyberheist/index.jsx
@@ -4,22 +4,22 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>CyberHeist - Wilder World</title>
+        <title>#CyberHeist - Wilder World</title>
         <link rel="icon" href="/favicon.ico" />
 
         <meta property="og:url" content="https://www.wilderworld.com/cyberheist/" />
-        <meta property="og:title" content="CyberHeist - Wilder World" />
+        <meta property="og:title" content="#CyberHeist by Wilder World" />
         <meta
           property="og:image"
-          content="https://d36eyd5j1kt1m6.cloudfront.net/user-assets/1753749/EcTBqHZBcoPL9tup/screen-shot-2021-04-20-at-10.01.21-am.png"
+          content="https://d36eyd5j1kt1m6.cloudfront.net/user-assets/1753749/jf9pyVW3Z2WXzHPS/cyber-heist-.jpeg"
         />
         <meta property="twitter:card" content="summary_large_image" />
         <meta
           property="twitter:image:src"
-          content="https://d36eyd5j1kt1m6.cloudfront.net/user-assets/1753749/EcTBqHZBcoPL9tup/screen-shot-2021-04-20-at-10.01.21-am.png"
+          content="https://d36eyd5j1kt1m6.cloudfront.net/user-assets/1753749/jf9pyVW3Z2WXzHPS/cyber-heist-.jpeg"
         />
         <meta property="fb:app_id" content="152351391599356" />
-        <meta property="og:description" content="The journey begins" />
+        <meta property="og:description" content="Complete the heists and win limited editions Wilder NFTs" />
       </Head>
       <style
         dangerouslySetInnerHTML={{
@@ -32,7 +32,7 @@ export default function Home() {
       />
       <div className="w-full h-full min-h-screen">
         <iframe
-          src="https://gleam.io/0jaiQ/cyberheist"
+          src="https://gleam.io/r6iil/cyberheist"
           frameBorder="0"
           allowFullScreen
           className="absolute w-full h-full"
