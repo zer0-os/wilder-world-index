8bb53abc6792996828a41f78696e31bd2e085288
Akshat Mittal <itsreallyakshat@gmail.com>
2021-05-11 21:03:21 +0530

    Add contract & hide section

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -24,7 +24,7 @@ export default function Home() {
         <FeaturedArt />
         <Projects />
         <Token />
-        <PoweredByNFTs />
+        {/* <PoweredByNFTs /> */}
         <LiquidArtMarket />
         <FinalCTA />
         <Footer />
