2930575c5e340d9ee19e1c66b638a6813566ff29
Akshat Mittal <itsreallyakshat@gmail.com>
2021-06-08 18:01:17 +0530

    Jake Paul's Cyberheist

--- src/pages/cyberheist/index.jsx
+++ src/pages/cyberheist/index.jsx
@@ -8,20 +8,20 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
 
         <meta property="og:url" content="https://www.wilderworld.com/cyberheist/" />
-        <meta property="og:title" content="Cyber Truck #CyberHeist by Wilder World" />
+        <meta property="og:title" content="Jake Paul&#39;s Lamborghini #CyberHeist" />
         <meta
           property="og:image"
-          content="https://d36eyd5j1kt1m6.cloudfront.net/user-assets/1753749/otrEU3unasOvnXEn/cyber-truck---under-2-mb.png"
+          content="https://d36eyd5j1kt1m6.cloudfront.net/user-assets/1753749/F9wkJNCKT4pZJeTG/upload.jpg"
         />
         <meta property="twitter:card" content="summary_large_image" />
         <meta
           property="twitter:image:src"
-          content="https://d36eyd5j1kt1m6.cloudfront.net/user-assets/1753749/otrEU3unasOvnXEn/cyber-truck---under-2-mb.png"
+          content="https://d36eyd5j1kt1m6.cloudfront.net/user-assets/1753749/F9wkJNCKT4pZJeTG/upload.jpg"
         />
         <meta property="fb:app_id" content="152351391599356" />
         <meta
           property="og:description"
-          content="💰 Complete the heists for your chance to win 💰 💎 1/9 Limited edition #CyberHeist NFTs 💎 🤖 Cyber Truck that you can cruise around in the Metaverse 🤖"
+          content="💰💰 Complete the heists for your chance to win 💰 💎 1/1 Limited edition #CyberHeist NFTs 💎 🏎 Jake Paul's Lamborghini that you can race around in the Metaverse 🏎"
         />
       </Head>
       <style
@@ -35,7 +35,7 @@ export default function Home() {
       />
       <div className="w-full h-full min-h-screen">
         <iframe
-          src="https://gleam.io/rmBgy/cyberheist"
+          src="https://gleam.io/RiFI9/jake-paul-cyberheist"
           frameBorder="0"
           allowFullScreen
           className="absolute w-full h-full"
