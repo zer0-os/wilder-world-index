fe65f1b00311d8c85466d633fadf1da29c3b3aa2
Akshat Mittal <itsreallyakshat@gmail.com>
2021-08-13 23:41:23 +0530

    Dynamic

--- src/pages/api/token/wild/get.js
+++ src/pages/api/token/wild/get.js
@@ -1,6 +1,6 @@
 export default async (req, res) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Cache-Control", "s-maxage=2592000, stale-while-revalidate");
+  res.setHeader("Cache-Control", `s-maxage=${60 * 60 * 12}, stale-while-revalidate`);
 
   if (!req.query.q) {
     return res.status(403).send("Invalid parameters");
@@ -9,7 +9,11 @@ export default async (req, res) => {
   if (req.query.q === "totalSupply") {
     return res.status(200).send("500000000.000000000000000000");
   } else if (req.query.q === "circulatingSupply") {
-    return res.status(200).send("44963612.810000000000000000");
+    return await fetch("https://sheetdb.io/api/v1/mahae67q9x371")
+      .then((e) => e.json())
+      .then((e) => {
+        return res.status(200).send(Number(e[9]["Vested"].split(",").join("")).toFixed(18).toString());
+      });
   }
 
   return res.status(403).send("Invalid query");
