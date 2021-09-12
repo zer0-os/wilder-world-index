0f4ab75ddb1957a3640943c1165c6aa75345750f
Akshat Mittal <itsreallyakshat@gmail.com>
2021-09-12 09:25:52 +0530

    Cyberheist

--- src/pages/cyberheist/index.jsx
+++ src/pages/cyberheist/index.jsx
@@ -4,24 +4,24 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>#CyberHeist - Wilder World</title>
+        <title>AirWILD Metaverse Kicks NFT #CyberHeist</title>
         <link rel="icon" href="/favicon.ico" />
 
         <meta property="og:url" content="https://info.wilderworld.com/cyberheist/" />
-        <meta property="og:title" content="Jake Paul&#39;s Lamborghini #CyberHeist" />
+        <meta property="og:title" content="AirWILD #CyberHeist" />
         <meta
           property="og:image"
-          content="https://d36eyd5j1kt1m6.cloudfront.net/user-assets/1753749/F9wkJNCKT4pZJeTG/upload.jpg"
+          content="https://d36eyd5j1kt1m6.cloudfront.net/user-assets/1753749/BJrE76z2vNijUzDo/airwild.jpeg"
         />
         <meta property="twitter:card" content="summary_large_image" />
         <meta
           property="twitter:image:src"
-          content="https://d36eyd5j1kt1m6.cloudfront.net/user-assets/1753749/F9wkJNCKT4pZJeTG/upload.jpg"
+          content="https://d36eyd5j1kt1m6.cloudfront.net/user-assets/1753749/BJrE76z2vNijUzDo/airwild.jpeg"
         />
         <meta property="fb:app_id" content="152351391599356" />
         <meta
           property="og:description"
-          content="💰💰 Complete the heists for your chance to win 💰 💎 1/1 Limited edition #CyberHeist NFTs 💎 🏎 Jake Paul's Lamborghini that you can race around in the Metaverse 🏎"
+          content="&#128176; Complete the heists for your chance to win &#128176;&#128142; 3: 1 of 1 Limited edition #CyberHeist NFTs &#128142;&#128095; AirWILD Kicks: Metaverse ready sneakers NFTs  &#128095;"
         />
       </Head>
       <style
@@ -35,7 +35,7 @@ export default function Home() {
       />
       <div className="w-full h-full min-h-screen">
         <iframe
-          src="https://gleam.io/RiFI9/jake-paul-cyberheist"
+          src="https://gleam.io/OOE7T/airwild-metaverse-kicks-nft-cyberheist"
           frameBorder="0"
           allowFullScreen
           className="absolute w-full h-full"
